00163d86e773ca655f5534ffd3418b857c4534d3
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-09-10 12:56:30 +0100

    adding customer to database

--- src/main/java/com/day9exercise/demo/Customer/CustomerRepository.java
+++ src/main/java/com/day9exercise/demo/Customer/CustomerRepository.java
@@ -1,5 +1,60 @@
 package com.day9exercise.demo.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-public interface CustomerRepository {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+@Repository("fakeData")
+public class CustomerRepository {
+
+    private static List<Customer> customerDb = new ArrayList<>();
+
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public CustomerRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public void customerDb(){
+        Customer customer1 = new Customer("Henry","Horrid", LocalDate.of(1994, 12, 10), "AHJSFKHAS");
+        Customer customer2 = new Customer("Sara","Jones", LocalDate.of(1999, 8, 24), "AHJSFKHAS");
+        customerDb.add(customer1);
+        customerDb.add(customer2);
+
+    }
+
+    //GET REQUEST
+    public List<Customer> selectAllCustomers(){
+//        return customerDb;
+        String sql = "SELECT * FROM customer";
+        List<Customer> results = jdbcTemplate.queryForList(sql, Customer.class);
+        return results;
+    }
+
+//    public int insertCustomer(UUID id, Customer newCustomer) {
+//        customerDb.add(id, newCustomer);
+//        return 1;
+//    }
+
+
+    //GET REQUEST
+    Optional<Customer> selectedCustomer(Integer id){
+        return customerDb.stream()
+                .filter(customer -> customer.getId() == id)
+                .findFirst();
+    }
+
+    //POST REQUEST
+    public int insertNewCustomer(Customer newCustomer){
+        customerDb.add(newCustomer);
+        String sql = "INSERT INTO customer(firstName, lastName, dob, age, passport) VALUES(?, ?, ?, ?, ?)";
+        int result = jdbcTemplate.update(sql, newCustomer.getFirstName(), newCustomer.getLastName(), newCustomer.getDob(), newCustomer.getAge(), newCustomer.getPassport());
+        return result;
+    }
 }
--- src/main/java/com/day9exercise/demo/DemoApplication.java
+++ src/main/java/com/day9exercise/demo/DemoApplication.java
@@ -1,16 +1,39 @@
 package com.day9exercise.demo;
-
+import com.day9exercise.demo.Customer.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.day9exercise.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		Start start = new Start();
-		start.welcome();
+		}
+
+		@Bean
+		CommandLineRunner commandLineRunner(CustomerRepositoryPostgres customerRepositoryPostgres){
+		return args -> {
+			Customer newCustomer = new Customer("Harry", "Jones", LocalDate.of(1994, 12,21), "JONES19027490124");
+			customerRepositoryPostgres.save(newCustomer);
+		};
+
+//		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+//		CustomerRepository customerRepository = new CustomerRepository(jdbcTemplate);
+//		CustomerService customerService = new CustomerService(customerRepository);
+//		CustomerController customerController = new CustomerController(customerService);
+//
+//		customerController.listCustomers();
+//
+//		Start start = new Start(customerController);
+//		start.welcome(customerRepository);
 	}
 
 }
