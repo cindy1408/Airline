d3abfb35fbfbb55125b6daff4d4fbf8ea9975202
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-09-10 15:53:17 +0100

    userput input save to database

--- src/main/java/com/day9exercise/demo/DemoApplication.java
+++ src/main/java/com/day9exercise/demo/DemoApplication.java
@@ -1,39 +1,58 @@
 package com.day9exercise.demo;
 import com.day9exercise.demo.Customer.*;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.day9exercise.demo"})
+
 public class DemoApplication {
+	private static CustomerController customerController;
+
+	private CustomerService customerService;
+
+	public DemoApplication(CustomerController customerController) {
+		this.customerController = customerController;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Would you like to book a flight? y/n");
+
+		String answer = scanner.nextLine();
+		if (answer.toLowerCase().trim().equals("y")) {
+			System.out.println("We will need to take in your information\nWhat is your first name?");
+			String firstName = scanner.nextLine();
+			System.out.println("What is your surname?");
+			String surname = scanner.nextLine();
+			System.out.println("What is your date of birth? \nPlease provide it in this format yyyy-mm-dd");
+			String dobRaw = scanner.nextLine();
+			LocalDate dob = LocalDate.parse(dobRaw);
+			System.out.println("Please provide your passport number");
+			String passport = scanner.nextLine();
+			Customer newCustomer = new Customer(firstName, surname, dob, passport);
+			System.out.println("Your information have been saved");
+			System.out.println(newCustomer);
+			customerController.listCustomers();
+			customerController.addNewCustomer(newCustomer);
+
+		} else {
+			System.out.println("Thank you for your time.");
 		}
 
-		@Bean
-		CommandLineRunner commandLineRunner(CustomerRepositoryPostgres customerRepositoryPostgres){
-		return args -> {
-			Customer newCustomer = new Customer("Harry", "Jones", LocalDate.of(1994, 12,21), "JONES19027490124");
-			customerRepositoryPostgres.save(newCustomer);
-		};
 
-//		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-//		CustomerRepository customerRepository = new CustomerRepository(jdbcTemplate);
-//		CustomerService customerService = new CustomerService(customerRepository);
-//		CustomerController customerController = new CustomerController(customerService);
-//
-//		customerController.listCustomers();
-//
-//		Start start = new Start(customerController);
-//		start.welcome(customerRepository);
-
+		//BELOW WORKS!
+//		@Bean
+//		CommandLineRunner commandLineRunner(CustomerRepositoryPostgres customerRepositoryPostgres){
+//		return args -> {
+//			Customer jessica = new Customer("Jessica", "Ali", LocalDate.of(2000, 6,14), "ALI9219027490124");
+//			customerRepositoryPostgres.save(jessica);
+//		};
 	}
-
 }
+
--- src/main/java/com/day9exercise/demo/Start.java
+++ src/main/java/com/day9exercise/demo/Start.java
@@ -1,23 +1,28 @@
 package com.day9exercise.demo;
 
-import com.day9exercise.demo.Customer.Customer;
-import com.day9exercise.demo.Customer.CustomerController;
-import com.day9exercise.demo.Customer.CustomerRepository;
-import com.day9exercise.demo.Customer.CustomerRepositoryPostgres;
+import com.day9exercise.demo.Customer.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.Scanner;
 
+@Component
 public class Start {
+    private CustomerService customerService;
     @Autowired
-    private CustomerController customerController;
+    CustomerController customerController = new CustomerController(customerService);
     Scanner scanner = new Scanner(System.in);
 
     public Start(CustomerController customerController) {
         this.customerController = customerController;
     }
-    public void welcome(CustomerRepositoryPostgres postgresql){
+
+    public Start() {
+
+    }
+
+    public void welcome() {
         System.out.println("Would you like to book a flight? y/n");
 //        scanner.nextLine();
         String answer = scanner.nextLine();
@@ -34,6 +39,7 @@ public class Start {
             Customer newCustomer = new Customer(firstName, surname, dob,passport);
             System.out.println("Your information have been saved");
             System.out.println(newCustomer);
+            customerController.listCustomers();
             customerController.addNewCustomer(newCustomer);
 
         } else {
