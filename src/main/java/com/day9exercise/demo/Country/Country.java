ae2c93e65d73ee237ee0d2993ca81da07d8d4a6b
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-09-11 17:45:16 +0100

    country class/controller/service/repository/database

--- src/main/java/com/day9exercise/demo/Country/Country.java
+++ src/main/java/com/day9exercise/demo/Country/Country.java
@@ -1,12 +1,50 @@
 package com.day9exercise.demo.Country;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity(name = "Country")
+@Table(name = "country", uniqueConstraints = {
+        @UniqueConstraint(name = "country_name", columnNames = "country_name")
+})
 public class Country {
+    @Id
+    @SequenceGenerator(name="country_sequence",
+            sequenceName = "country_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE, generator = "country_sequence"
+    )
+    @Column(name = "id", updatable = false)
+    private int id;
+    @Column(name = "country_name", nullable = false, columnDefinition = "TEXT")
     private String name;
+    @Column(name = "estimated_travel", nullable = false)
+    private int estimatedTravelMinutes;
+    private double price;
+
+    public Country(@JsonProperty("name") String name,
+                   @JsonProperty("estimated_travel") int estimatedTravelMinutes, double price) {
+        this.name = name;
+        this.estimatedTravelMinutes = estimatedTravelMinutes;
+        this.price = price;
+    }
 
     public Country(String name) {
         this.name = name;
+    }
+
+    public Country() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -17,23 +55,39 @@ public class Country {
         this.name = name;
     }
 
+    public int getEstimatedTravelMinutes() {
+        return estimatedTravelMinutes;
+    }
+
+    public void setEstimatedTravelMinutes(int estimatedTravel) {
+        this.estimatedTravelMinutes = estimatedTravelMinutes;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Country country = (Country) o;
-        return Objects.equals(name, country.name);
+        return id == country.id && Double.compare(country.estimatedTravelMinutes, estimatedTravelMinutes) == 0 && Objects.equals(name, country.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(id, name, estimatedTravelMinutes);
     }
 
     @Override
     public String toString() {
         return "Country{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", estimatedTravelMinutes=" + estimatedTravelMinutes +
                 '}';
     }
 }
---
+++ src/main/java/com/day9exercise/demo/Country/CountryService.java
@@ -0,0 +1,102 @@
+package com.day9exercise.demo.Country;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Scanner;
+
+@Service
+public class CountryService {
+    private final CountryRepositoryPostgres countryRepositoryPostgres;
+    Scanner scanner = new Scanner(System.in);
+
+    @Autowired
+    public CountryService(CountryRepositoryPostgres countryRepositoryPostgres) {
+        this.countryRepositoryPostgres = countryRepositoryPostgres;
+    }
+
+    //GET REQUEST
+    public List<Country> getAllCountries(){
+        return countryRepositoryPostgres.findAll();
+    }
+
+    //GET REQUEST
+    public void requestedCountry(String countryName){
+        countryRepositoryPostgres.findCountryByName(countryName)
+                .ifPresentOrElse(country -> {
+                    System.out.println(country);
+                }, () -> {
+                    System.out.println("The country id does not exist within our system.");
+                });
+    }
+
+
+    //POST REQUEST
+    public Country addNewCountry(Country newCountry){
+        return countryRepositoryPostgres.save(newCountry);
+    }
+
+    //PUT REQUEST
+    public void updateCountry(int countryId, int requestedUpdate){
+        countryRepositoryPostgres.findById(countryId)
+                .ifPresentOrElse(country -> {
+                    switch(requestedUpdate){
+                        case 1:
+                            System.out.println("Current Country name is " + country.getName() + "\nwould you like to change it?y/n");
+                            String ans = scanner.nextLine();
+                            if(ans.toLowerCase().trim().equals("y")){
+                                System.out.println("Please enter the updated name of the country");
+                                String updatedName = scanner.nextLine();
+                                country.setName(updatedName);
+                                countryRepositoryPostgres.save(country);
+                                System.out.println("Thank you, the country name has been updated to " + country.getName());
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Current estimated time is " + country.getEstimatedTravelMinutes() + "\nWould you like to change it?y/n");
+                            String input = scanner.nextLine();
+                            if(input.toLowerCase().trim().equals("y")){
+                                System.out.println("Please enter the estimated duration in minutes");
+                                int updatedEstimatedDuration = scanner.nextInt();
+                                country.setEstimatedTravelMinutes(updatedEstimatedDuration);
+                                countryRepositoryPostgres.save(country);
+                                System.out.println("Thank you, the country estimated duration has been updated to " + country.getEstimatedTravelMinutes());
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Current price is " + country.getPrice() + "\nWould you like to change it?y/n");
+                            String userInput = scanner.nextLine();
+                            if(userInput.toLowerCase().trim().equals("y")){
+                                System.out.println("Please enter the updated price");
+                                double updatedPrice = scanner.nextDouble();
+                                country.setPrice(updatedPrice);
+                                countryRepositoryPostgres.save(country);
+                                System.out.println("Thank you, the price has been updated to " + country.getPrice());
+                            }
+                            break;
+                        default:
+                            System.out.println("Please select one of the available options.");
+                    }
+                }, () -> {
+                    System.out.println(countryId + " cannot be found in our database.");
+                });
+    }
+
+    //DELETE REQUEST
+    public void deleteCountry(int countryId){
+        countryRepositoryPostgres.findById(countryId)
+                .ifPresentOrElse(country -> {
+                    System.out.println("Are you sure you want to delete " + country + "?");
+                    String ans = scanner.nextLine();
+                    if(ans.toLowerCase().trim().equals("y")){
+                        countryRepositoryPostgres.delete(country);
+                        System.out.println(country + " has been successfully deleted.");
+                    } else {
+                        System.out.println(country + " has not been deleted.");
+                    }
+                }, () -> {
+                    System.out.println("Country id is not registered in our system");
+                });
+    }
+}
